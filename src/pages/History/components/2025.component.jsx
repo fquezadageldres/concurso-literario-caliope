5849465f9f35aace97262bc2d921a5822c667445
sandrade <sebastian.andrade@dimarsa.cl>
2025-04-23 20:21:18 -0400

    "Update 2025 contest theme, rules, and formats; add new artistic category"

--- src/pages/History/components/2025.component.jsx
+++ src/pages/History/components/2025.component.jsx
@@ -31,10 +31,10 @@ export const Version2025 = () => {
                 } eventKey={1}>
                     <div className='mx-12'>
                         <p>Nombre: <span className='text-[#999] ml-2'>10º Concurso Regional de Literatura Calíope</span></p>
-                        <p>Tema: <span className='text-[#999] ml-2'>Unidad y Diversidad</span></p>
-                        <p>Formato literario: <span className='text-[#999] ml-2'>Cuentos y Poesías</span></p>
-                        <p>Fecha de entrega: <span className='text-[#999] ml-2'>9 de marzo 2025 - 1 de junio 2025</span></p>
-                        <p>Fecha de premiación: <span className='text-[#999] ml-2'>15 de junio 2025 19:00hrs</span></p>
+                        <p>Tema: <span className='text-[#999] ml-2'>Mitología de los pueblos originarios Chilenos</span></p>
+                        <p>Formato: <span className='text-[#999] ml-2'>Poesías y Obras plásticas</span></p>
+                        <p>Fecha de entrega: <span className='text-[#999] ml-2'>01 abril 2025 - 31 de mayo 2025</span></p>
+                        <p>Fecha de premiación: <span className='text-[#999] ml-2'>18 de junio 2025 19:00hrs</span></p>
                         <p>Lugar de premiación: <span className='text-[#999] ml-2'>Corporación Cultural Nueva Acrópolis Puerto Montt - Anibal Pinto #297</span></p>
                     </div>
                 </Accordion.Panel>
@@ -54,33 +54,42 @@ export const Version2025 = () => {
                     />
                 } eventKey={3}>
                 <div  className='mx-12'>
-                <ul className="flex flex-col gap-3 md:px-10 mt-5">
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Podrán participar personas de toda edad con residencia en la región de Los Lagos.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Deberá considerarse en las obras el o los temas siguientes: “Unidad y Diversidad”.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los concursantes podrán elegir para expresarse, cualquiera de los siguientes géneros:</li>
-                        <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Género narrativo, en su expresión de cuento.</li>
-                        <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Género lírico en su expresión poesía.</li>
+
+                    <ul className="flex flex-col gap-3 md:px-10">
+                        <div className="border-b border-[#ddd] my-2"></div>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Podrán participar personas mayores de edad con residencia en la región de Los
+                            Lagos y de la región de Los Ríos.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Deberá considerarse para la realización de la obra plástica o poética el
+                            siguiente tema: “Mitología de los pueblos originarios Chilenos”.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los concursantes podrán elegir uno de los siguientes géneros artísticos para
+                            desarrollar el tema:</li>
+                        <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Poesía.</li>
+                        <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Obra plástica.</li>
                         <div className="border-b border-[#ddd] my-2"></div>
                         <div className="mt-0 ml-0">
-                            <li className="ml-10 mb-2">Cuento</li>
-                            <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> La extensión del cuento deberá tener un máximo de diez carillas, letra Arial N°12, tamaño carta a doble espacio.</li>
-                        </div>
-                        <div className="mt-5 ml-0">
-                            <li className="ml-10 mb-2">Poesia</li>
+                            <li className="ml-10 mb-2">Poesía</li>
                             <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> La estructura y métrica del poema es libre.</li>
                             <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Los caracteres deben estar en letra Arial N°12, tamaño carta doble espacio.</li>
+                        </div>
+                        <div className="mt-5 ml-0">
+                            <li className="ml-10 mb-2">Obra plástica</li>
+                            <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Las técnicas a emplear podrán ser: Óleo, Acrílico, Acuarela, Tempera o Grabado, siempre y cuando respeten las condiciones bidimensionales y de tamaño detalladas a continuación.</li>
+                            <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Formato de entrega: Las obras no podrán exceder los 1.20 x 1.20 m. ni ser inferior a 0.90 x 0.90 m.</li>
+                            <li className="inline-flex items-start gap-3 ml-10"><div className="w-[20px]"><IoMdCheckmark className="!text-[18px] text-lLightGreen"/></div> Formato acuarelas: 0.50 x 0.70 cm.</li>
                         </div>
                     </ul>
                     <div className="border-b border-[#ddd] my-2 "></div>
                     <ul className="flex flex-col gap-3 md:px-10">
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los trabajos deberán ser completamente inéditos y no haber participado en otros concursos, esta causal o bien plagio, será motivo de eliminación. Las obras tendrán que ser enviadas en forma digital, a través del correo electrónico de Nueva Acrópolis Puerto Montt: puertomontt@nueva-acropolis.cl en formato word o PDF, deberá incluir nombre completo del concursante, número de contacto y correo electronico personal.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los trabajos deberán ser completamente inéditos y no haber participado en otros concursos, esta causal o bien el plagio de la obra de un tercero, será motivo de eliminación. La categoría poesía deberá enviar su trabajo en forma digital, a través del correo electrónico de Nueva Acrópolis Puerto Montt: puertomontt@nueva-acropolis.cl en formato word o PDF, deberá incluir nombre completo del concursante, número de contacto y correo electrónico personal. La categoría obra plástica será recepcionada en nuestra sede ubicada en Aníbal Pinto #297. El autor se hace responsable del envío y devolución de su obra. La obra no debe venir firmada ni señalada con nombre, ni título del cuadro. La identificación debe venir en sobre cerrado adjunto con nombre, dirección, teléfono, correo electrónico, cédula de identidad y nombre de la obra.</li>
                         <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los ganadores serán contactados por la misma vía, a los cuales se les solicitará, fotocopia de su cédula de identidad, por ambos lados y un comprobante de domicilio (boleta de luz, agua o certificado), con la finalidad de corroborar su residencia regional.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los concursantes podrán participar solo con un título por género literario.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El plazo de recepción de las obras es a contar del 9 de Marzo de 2025 hasta el 1 de junio de 2025 inclusive, posterior a eso quedarán fuera del concurso. Los trabajos deberán tener fecha de envío anterior o igual a la fecha de cierre del concurso.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> La premiación se realizará en un acto público el día 15 de Junio a las 19:00 hrs en la sede de la Corporación Cultural Nueva Acrópolis Puerto Montt, ubicada en calle Anibal Pinto #297.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El jurado estará constituido por dos especialistas del ámbito, cuya identidad será revelada en el momento de la premiación. Los señores jurados podrán declarar desierto el concurso en cualquiera de los dos géneros, si lo estimaran necesario, por falencia en el cumplimiento de las bases u otro motivo justificado.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Habrá solo un premio, por cada género literario, que constará de libros donados por los patrocinadores: “Biblioteca Regional de Los Lagos” y “Libros Sotavento”, más un galardón por género literario premiado y un diploma para los ganadores y para el escritor más joven.</li>
-                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El autor al participar da autorización explícita a la organización quien se reserva el derecho de publicación en sus medios de difusión asociados para conocimiento público de las obras.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Los concursantes podrán participar solo con un título por género.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El plazo de recepción de las obras es a contar de 01 abril hasta el 31 de mayo inclusive, posterior a eso quedarán fuera del concurso. Los trabajos deberán tener fecha de envío anterior o igual a la fecha de cierre del concurso.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> La premiación se realizará en un acto público el día 18 de junio a las 19:00 hrs en la sede de la Corporación Cultural Nueva Acrópolis Puerto Montt, ubicada en calle Aníbal Pinto #297.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El jurado estará constituido por 2 especialistas del ámbito, más la opinión de la organización, cuya identidad será revelada en el momento de la premiación. Los señores jurados podrán declarar desierto el concurso en cualquiera de los 2 géneros, si lo estimaran necesario, por falencia en el cumplimiento de las bases u otro motivo justificado.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> Habrá solo un ganador por cada género, para la categoría de poesía el premio constará de libros donados por las entidades colaborativas: “Biblioteca Regional Puerto Montt”, “Libros Sotavento”, “Biblioteca Puerto Varas” más un galardón. Y el segundo lugar recibirá una mención honrosa que consiste en un diploma reconociendo su participación. El premio para el ganador en categoría Obra Plástica será un galardón, y materiales de bellas artes donados por las entidades colaborativas “Galería compañía de artes” y “Librería Patagonia”, más un libro. El segundo lugar recibirá una mención honrosa reconociendo su participación.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> El autor al participar da autorización explícita a la organización quien se reserva el derecho de publicación en sus medios de difusión asociados para conocimiento público de las obras, tanto de poesía como obra plástica. Además, se ofrece la disposición de espacio en la organización cultural Nueva Acrópolis para exponer al público en general, las obras tanto poesía como obra plástica por el periodo de un mes.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> La decisión del jurado será inapelable.</li>
+                        <li className="inline-flex items-start gap-3"><div className="w-[20px]"><GoChecklist className="!text-[24px] text-lLightGreen" /></div> La inscripción en el concurso implica la aceptación total de las bases.</li>
                     </ul>
                 </div>
                 </Accordion.Panel>
@@ -88,7 +97,7 @@ export const Version2025 = () => {
             </div>
             <Modal overflow={false} size={"lg"} backdrop={true} keyboard={false} open={open} onClose={handleClose}>
                 <Modal.Header className='mb-3'>
-                    <Modal.Title>Fotos 9º Concurso Regional de Literatura Calíope</Modal.Title>
+                    <Modal.Title>Fotos 10º Concurso Interregional Poesía - Pintura Calíope</Modal.Title>
                 </Modal.Header>
                 <Modal.Body className='flex flex-col overflow-scroll gap-5 max-h-[600px] md:max-h-[800px]'>
                         <img src={Img0} alt="" />
